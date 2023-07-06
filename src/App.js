c5207eb1c260f5411b3cf5ea1a50b6b8a2efb7cd
jmjy1115 <108736255+jmjy1115@users.noreply.github.com>
2023-07-07 01:46:51 +0900

    04-3 컴포넌트에 key props 추가하기

--- src/App.js
+++ src/App.js
@@ -4,42 +4,45 @@ function Movie({ title, image }) {
   return (
     <>
       <h1>'{title}'</h1>
-      <img src={image} alt='moiveImage'/>
+      <img src={image} alt={title} />
     </>
   );
 }
 
 const movieList = [
   {
+    id: 1,
     title: 'Interstellar',
     image: '/image0.png',
   },
   {
+    id: 2,
     title: 'The Flash',
     image: '/image1.png',
   },
   {
+    id: 3,
     title: 'Elemental',
     image: '/image2.png',
   },
   {
+    id: 4,
     title: 'Transformers',
     image: '/image3.png',
   },
   {
+    id: 5,
     title: 'Guardians of the Galaxy',
     image: '/image4.png',
   },
 ]
 
-function renderMovie(movie) {
-  return <Movie title={movie.title} image={movie.image} />
-}
-
 function App() {
   return (
     <div>
-      {movieList.map(renderMovie)}
+      {movieList.map(movie => (
+      <Movie key={movie.id} title={movie.title} image={movie.image} />
+      ))}
     </div>
   );
 }
