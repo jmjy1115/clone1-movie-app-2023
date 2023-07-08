caddc012605ed3b71e856a278b4f8cca4e85cf6b
jmjy1115 <108736255+jmjy1115@users.noreply.github.com>
2023-07-08 12:50:48 +0900

    04-4 prop-types 활용하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Movie({ title, image }) {
+function Movie({ title, image, rating }) {
   return (
     <>
       <h1>'{title}'</h1>
+      <h4>평점: {rating.toFixed(1)}/5.0</h4>
       <img src={image} alt={title} />
     </>
   );
@@ -14,26 +16,31 @@ const movieList = [
     id: 1,
     title: 'Interstellar',
     image: '/image0.png',
+    rating: 3.0,
   },
   {
     id: 2,
     title: 'The Flash',
     image: '/image1.png',
+    rating: 4.3,
   },
   {
     id: 3,
     title: 'Elemental',
     image: '/image2.png',
+    rating: 3.7,
   },
   {
     id: 4,
     title: 'Transformers',
     image: '/image3.png',
+    rating: 4.5,
   },
   {
     id: 5,
     title: 'Guardians of the Galaxy',
     image: '/image4.png',
+    rating: 4.2,
   },
 ]
 
@@ -41,10 +48,16 @@ function App() {
   return (
     <div>
       {movieList.map(movie => (
-      <Movie key={movie.id} title={movie.title} image={movie.image} />
+      <Movie key={movie.id} title={movie.title} image={movie.image} rating={movie.rating} />
       ))}
     </div>
   );
+}
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
 }
 
 export default App;
