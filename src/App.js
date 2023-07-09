fd2030c8744e397cd9b0b6ef4fa2d3cbbc73697f
jmjy1115 <108736255+jmjy1115@users.noreply.github.com>
2023-07-09 23:20:20 +0900

    05-2 state와 setState()를 사용하여 증감 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,63 +1,31 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Movie({ title, image, rating }) {
-  return (
-    <>
-      <h1>'{title}'</h1>
-      <h4>평점: {rating.toFixed(1)}/5.0</h4>
-      <img src={image} alt={title} />
-    </>
-  );
-}
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
 
-const movieList = [
-  {
-    id: 1,
-    title: 'Interstellar',
-    image: '/image0.png',
-    rating: 3.0,
-  },
-  {
-    id: 2,
-    title: 'The Flash',
-    image: '/image1.png',
-    rating: 4.3,
-  },
-  {
-    id: 3,
-    title: 'Elemental',
-    image: '/image2.png',
-    rating: 3.7,
-  },
-  {
-    id: 4,
-    title: 'Transformers',
-    image: '/image3.png',
-    rating: 4.5,
-  },
-  {
-    id: 5,
-    title: 'Guardians of the Galaxy',
-    image: '/image4.png',
-    rating: 4.2,
-  },
-]
+  add = () => {
+    this.setState(current => ({
+      count: current.count + 1,
+    }));
+  }
 
-function App() {
-  return (
-    <div>
-      {movieList.map(movie => (
-      <Movie key={movie.id} title={movie.title} image={movie.image} rating={movie.rating} />
-      ))}
-    </div>
-  );
-}
+  minus = () => {
+    this.setState(current => ({
+      count: current.count - 1,
+    }));
+  }
 
-Movie.propTypes = {
-  title: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    ) 
+  }
 }
 
 export default App;
