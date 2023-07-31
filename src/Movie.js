15d640b87f082b8ce3125bd45bac95dc17adb779
jmjy1115 <108736255+jmjy1115@users.noreply.github.com>
2023-07-31 23:03:43 +0900

    06-4 HTML 추가, CSS 적용, 이미지 삽입

--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,23 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import './Movie.css';
 
-function Moive({ id, year, title, summary, poster }) {
-  return <h4>{title}</h4>;
+function Moive({ year, title, summary, poster }) {
+  return (
+    <div class="movie">
+      <img src={poster} alt={title} title={title}></img>
+      <div class="movie__data">
+        <h3 class="movie__title">
+          {title}
+        </h3>
+        <h3 class="movie__year">{year}</h3>
+        <p class="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 Moive.propTypes = {
-  id: PropTypes.number.isRequired,
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
