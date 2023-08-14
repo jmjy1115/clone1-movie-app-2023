ca3573377b13e3fdf4a91353747d0afe4d10a014
jmjy1115 <108736255+jmjy1115@users.noreply.github.com>
2023-08-14 23:54:24 +0900

    08-3 내비게이션 만들기 1

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './routes/Home';
 import About from './routes/About';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Routes>
         <Route path='/' Component={Home} />
         <Route path='/about' Component={About} />
